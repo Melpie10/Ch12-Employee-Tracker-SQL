ab1561c3a23a406d584e3bac95a2c78e0f3d4e31
melany14 <melany14@gmail.com>
2022-04-03 21:34:42 -0400

    pupulated seed

---
+++ db/seeds.sql
@@ -0,0 +1,41 @@
+SOURCE db/schema.sql;
+
+-- Table Values: Depts 
+INSERT INTO departments (department_name)
+VALUES
+    ("Office Administration"), 1
+    ("Human Resources"), 2
+    ("Disney Talent"), 3
+    ("Imageneers"), 4
+    ("Accounting"), 5
+    ("Park Operations"); 6
+
+-- Table Values: Roles 
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ("Disney-Park Character", 25000, 3),
+    ("Makeup Artist", 30000, 4),
+    ("Choreographer", 40000, 4),
+    ("Park Manager", 90000, 6),
+    ("Cleaning Crew", 23000, 6),
+    ("HR Director", 50000, 2),
+    ("Call Center Rep", 40000, 1),
+    ("Customer Service Supervisor", 43000, 1),
+    ("Customer Service", 37000, 1),
+    ("Accountant", 70000, 5),
+    ("Drawing Artist", 65000, 4);
+
+
+-- Table Values: Employees 
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES 
+    ("Lilo", "Stitch", 13, 23),
+    ("Snow", "White", 14, 17),
+    ("Sleeping", "Beauty", 12, 15),
+    ("Prince", "Charming", 9, 23),
+    ("Captain", "Hook", 15, 23),
+    ("Mickey", "Mouse", 1, NULL),
+
+
+-- Add mana Foreign Key AFTER  Employees table so  Foreign Key can reference PK in same table
+ALTER TABLE employees ADD CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id);
